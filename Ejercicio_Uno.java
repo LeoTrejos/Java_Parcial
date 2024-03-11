df61d1a2d64541b7f5691fb6d24a6b0854086dd0
Leo Trejos <hl.trejos28@ciaf.edu.co>
2024-03-11 01:56:25 -0500

    Actualizacion del Taller

--- Ejercicio_Uno.java
+++ Ejercicio_Uno.java
@@ -5,7 +5,7 @@ bisiesto o no. Un año bisiesto es aquel divisible por 4, excepto aquellos divis
 pero no por 400. Por ejemplo, 2000 y 2400 son años bisiestos, mientras que 1800, 1900
 y 2100 no lo son.  */
 
-public class Uno {
+public class Ejercicio_Uno {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Por favor, ingresa un nombre:");
